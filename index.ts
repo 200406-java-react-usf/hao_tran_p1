411d31957d60b7bcd446f546c00ae5b44b69199f
HaoTran <31514669+donxiya@users.noreply.github.com>
2020-05-08 16:55:28 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,14 @@
+  
+import express from 'express';
+
+let app = express();
+
+app.use('/', express.json());
+
+app.get('/', (req,resp) => {
+
+    resp.send('THIS WORKS');
+
+});
+
+app.listen(8080);
