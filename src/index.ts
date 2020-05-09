6443533ce4ff2507232ad7746cdae109bc483b54
donxiya <31514669+donxiya@users.noreply.github.com>
2020-05-08 22:23:10 -0500

    change server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
-  
+import dotenv from 'dotenv';
 import express from 'express';
+import fs from 'fs';
+import morgan from 'morgan';
+import path from 'path';
+// import { sessionMiddleware } from './middleware/session-middleware';
+// import { corsFilter } from './middleware/cors-filter';
+import { Pool } from 'pg';
 
-let app = express();
+// environment configuration
+dotenv.config();
 
-app.use('/', express.json());
-
-app.get('/', (req,resp) => {
-
-    resp.send('THIS WORKS');
-
+// database configuration
+export const connectionPool: Pool = new Pool({
+    host: process.env['DB_HOST'],
+    port: +process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    max: 5
 });
 
-app.listen(8080);
+// logging configuration
+fs.mkdir(`${__dirname}/logs`, () => {});
+const logStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
+
+// web server configuration
+const app = express();
+app.use(morgan('combined', { stream: logStream }));
+// app.use(sessionMiddleware);
+// app.use(corsFilter);
+app.use('/', express.json());
+//app.use('/users', UserRouter);
+
+
+app.listen(8080, () => {
+    console.log(`Application running and listening at: `);
+});
