661214a621115abd14c2b74a0057224ad7baaed9
donxiya <31514669+donxiya@users.noreply.github.com>
2020-05-19 13:03:44 -0500

    Update user-router.ts

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import AppConfig from '../config/app';
+import url from 'url';
+import { isEmptyObject } from '../util/validator';
+import { ParsedUrlQuery } from 'querystring';
 
 export const UserRouter = express.Router();
 
 const userService = AppConfig.userService;
 
-UserRouter.get('', async (req, res) => {
+UserRouter.get('/all', async (req, res) => {
     console.log("hit users")
 
     try {
@@ -14,4 +17,24 @@ UserRouter.get('', async (req, res) => {
     } catch (e) {
         res.status(400).json(e);
     }
+});
+
+UserRouter.get('', async (req, resp) => {
+
+    try {
+
+        let reqURL = url.parse(req.url, true);
+
+        if(!isEmptyObject<ParsedUrlQuery>(reqURL.query)) {
+            let payload = await userService.getUserByUniqueKey({...reqURL.query});
+            resp.status(200).json(payload);
+        } else {
+            let payload = await userService.getAllUsers();
+            resp.status(200).json(payload);
+        }
+
+    } catch (e) {
+        resp.status(e.statusCode).json(e);
+    }
+
 });
