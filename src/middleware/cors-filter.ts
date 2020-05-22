3634ca2b4082849cab1deb36cc7bab66209f1ce5
donxiya <31514669+donxiya@users.noreply.github.com>
2020-05-22 07:33:57 -0500

    added cors

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -2,12 +2,11 @@ import { Request, Response } from "express";
 
 
 export function corsFilter(req: Request, res: Response, next) {
-
-    res.header('Access-Control-Allow-Origin', '*'); 
+    //*
+    res.header('Access-Control-Allow-Origin', 'http://hao-tran-p1-ui.s3-website.us-east-2.amazonaws.com/'); 
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-
     // If this request is an OPTION request (aka "pre-flight request") send it back with a status of 200
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
